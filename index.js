0bd66864f6ffc94a99fe172f9f5a15da489910cc
maryam <Reemaali9048@gmail.com>
2023-09-21 00:19:53 +0300

    complete assigment

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
+multiply
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 100); 
+
+let num3 = 64;
+let num4 = 3;
+let mod = num3 % num4;
+
+let max = Math.max(5, 10, 15, 20);
 // Write your code here
